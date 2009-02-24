36cb7e42a9e7e29877e24a917ce2bf3ade8029eb
Sarah Taraporewalla <sarah.tarap@gmail.com>
2009-02-24 21:23:39 +0000

    template group directory now also looks for ruby files to see if there is a renderer available;

--- engine/spec/template_group_directory.rb
+++ engine/spec/template_group_directory.rb
@@ -1,11 +1,31 @@
 require 'engine/spec/helper'
 require 'tempfile'
+require 'active_support'
 
 describe Slippers::TemplateGroupDirectory do
-  it 'should find the file template in the directory folder' do
+  
+  it 'should find the file in the directory folder' do
     template_group = Slippers::TemplateGroupDirectory.new('engine/spec/views')
     template_group.find('index').should eql(Slippers::Engine.new('Hey foo', :template_group => template_group))
     template_group.find('person/age').should eql(Slippers::Engine.new('The age for him is $age$', :template_group => template_group))
   end
+
+  it 'should return nil if it cannot find the file in the directory folder' do
+    template_group = Slippers::TemplateGroupDirectory.new('engine/spec/views')
+    template_group.find('person/not_found').should eql(nil)
+  end
+
+  it 'should read the st template file and return a new slipers engine for it' do
+    template_group = Slippers::TemplateGroupDirectory.new('engine/spec/views')
+    template_group.find('index').should eql(Slippers::Engine.new('Hey foo', :template_group => template_group))
+    template_group.find('person/age').should eql(Slippers::Engine.new('The age for him is $age$', :template_group => template_group))
+  end
+  
+  it 'should load the ruby file found in the directory folder' do
+    template_group = Slippers::TemplateGroupDirectory.new('engine/spec/views')
+    template_group.find('person/date_renderer').class.should eql(DateRenderer)
+    template_group.find('money').class.should eql(Money)
+  end
+  
 
 end
--- engine/template_group_directory.rb
+++ engine/template_group_directory.rb
@@ -1,15 +1,29 @@
-require 'engine/template_group'
-require 'engine/file_template'
+require 'engine/engine'
+
 module Slippers
   class TemplateGroupDirectory < TemplateGroup
     def initialize(directory_path)
       @directory_path = directory_path
     end
     attr_reader :directory_path
+    
     def find(subtemplate)
+
       file_name = @directory_path + '/' + subtemplate + '.st'
+      return find_renderer(subtemplate) unless File.exist?(file_name)
       Engine.new(FileTemplate.new(file_name).template, :template_group => self)
+      
     end
+    
+    def find_renderer(subtemplate)
+      file_name = @directory_path + '/' + subtemplate + '.rb'
+      return nil unless File.exist?(file_name)
+      renderer_name = subtemplate.split('/')[-1]
+      autoload(renderer_name.camelize.to_sym, file_name)
+      renderer_name.camelize.constantize.new
+    end
+    
+    
     def eql?(other)
       return false unless other
       directory_path.eql?(other.directory_path)
